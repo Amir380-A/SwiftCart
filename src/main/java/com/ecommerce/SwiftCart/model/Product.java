2ec1e1dcd7322453a96ce80bde102be9ed09e81f
Amir380-A <62997533+Amir380-A@users.noreply.github.com>
2024-10-13 20:21:31 +0300

    initial commit

---
+++ src/main/java/com/ecommerce/SwiftCart/model/Product.java
@@ -0,0 +1,19 @@
+package com.ecommerce.SwiftCart.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+}
